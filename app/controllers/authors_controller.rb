8cff56b36a893832bcdebc576565bfa975a05e6b
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-01 15:53:53 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    set_author
   end
 
   def new
@@ -8,16 +8,36 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
     if @author.valid?
       @author.save
-    redirect_to author_path(@author)
-  else
+      redirect_to author_path(@author)
+    else
     render :new
+    end
   end
-end
+
+  def edit
+    set_author
+  end
+
+  def update
+    set_author
+    @author.update(author_params)
+    if @author.valid?
+       @author.save
+    redirect_to author_path(@author)
+    else
+    render :edit
+    end
+  end
+
 
   private
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
